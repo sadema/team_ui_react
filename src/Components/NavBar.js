84413bdb6b14f5b16787952860c393fda8914b36
Sjoerd Adema <sjoerd@kristalsoftware.nl>
2020-10-07 17:34:29 +0200

    [master] navigation bar

---
+++ src/Components/NavBar.js
@@ -0,0 +1,113 @@
+import React, {Component, Fragment} from 'react';
+import {withStyles} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import {IconButton} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import Typography from "@material-ui/core/Typography";
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+import {Teams} from "../Pages/Teams";
+import {Players} from "../Pages/Players";
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1
+    },
+    flex: {
+        flex: 1
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20
+    },
+    toolbarMargin: theme.mixins.toolbar
+});
+
+const TeamsToolbar = withStyles(styles)(
+    class extends Component {
+        static defaultProps = {
+            MenuItems: () => (
+                <Fragment>
+                    <MenuItem component={Link} to="/teams">
+                        Teams
+                    </MenuItem>
+                    <MenuItem component={Link} to="/players">
+                        Players
+                    </MenuItem>
+                </Fragment>
+            )
+        };
+
+        state = {anchor: null};
+
+        closeMenu = () => this.setState({anchor: null});
+
+        render() {
+            const {classes, title, MenuItems} = this.props;
+
+            return (
+                <Fragment>
+                    <AppBar position="fixed">
+                        <Toolbar>
+                            <IconButton
+                                className={classes.menuButton}
+                                color="inherit"
+                                aria-label="Menu"
+                                onClick={e =>
+                                    this.setState({anchor: e.currentTarget})
+                                }
+                            >
+                                <MenuIcon/>
+                            </IconButton>
+                            <Menu
+                                anchorEl={this.state.anchor}
+                                open={Boolean(this.state.anchor)}
+                                onClose={this.closeMenu}>
+                                <MenuItems/>
+                            </Menu>
+                            <Typography
+                                variant="title"
+                                color="inherit"
+                                className={classes.flex}
+                            >
+                                {title}
+                            </Typography>
+                        </Toolbar>
+                    </AppBar>
+                    <div className={classes.toolbarMargin}/>
+                </Fragment>
+            );
+        }
+    }
+);
+
+export const NavBar = withStyles(styles)(({classes}) => (
+    <Router>
+        <div className={classes.root}>
+            <Route
+                exact
+                path="/"
+                render={() => (
+                    <Redirect to="/teams"/>
+                )}
+            />
+            <Route
+                path="/teams"
+                render={() => (
+                    <Fragment>
+                        <TeamsToolbar title="Teams"/>
+                        <Teams/>
+                    </Fragment>
+                )}
+            />
+            <Route
+                path="/players"
+            >
+                <TeamsToolbar title="Players"/>
+                <Players/>
+            </Route>
+        </div>
+    </Router>
+));
---
+++ src/Pages/Players.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Players = () => {
+    return (
+        <h1>Players</h1>
+    );
+};
